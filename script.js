0d031ad4779266e0f115fa8233a04b77a36a68a1
Sam <morgan.94.sam@gmail.com>
2020-10-25 16:17:21 +0000

    data validation complete

--- script.js
+++ script.js
@@ -13,7 +13,37 @@ function showModal() {
     websiteNameEl.focus();
 }
 
-// Event Listeners
+// Modal Event Listeners
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click',(e) => (e.target === modal ? modal.classList.remove('show-modal') : false)); // This dismisses a modal if clicking on the overlay
+
+//Validate Form
+function validateURL(nameValue,urlValue){
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+
+    if(!urlValue.match(regex)) {
+        alert('Please provide a valid url');
+        return false;
+    }
+    //Valid
+    return true;
+}
+
+// Store Bookmark
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteURLEl.value;
+    console.log(nameValue,urlValue);
+    if (!urlValue.includes('http://','https://')){
+        urlValue = `https://${urlValue}`;
+    }
+    if (!validateURL(nameValue,urlValue)){
+        return false;
+    }
+}
+
+// Event Listeners
+bookmarkForm.addEventListener('submit', storeBookmark);
